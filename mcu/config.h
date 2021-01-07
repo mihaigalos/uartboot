02c5a2bd4b193783583bf88e050d58bc674cf82b
Mihai Galos <mihaigalos@gmail.com>
2021-01-07 10:22:40 +0100

    Improve cohesion

--- mcu/config.h
+++ mcu/config.h
@@ -7,3 +7,22 @@
 
 static constexpr uint8_t source_i2c_address_for_program =
     0x50; // default address for CAT24M01 I2C EEPROM
+
+#ifndef TESTING
+#include <avr-bootloader-common/all.h>
+#include <util/delay.h>
+
+#include "software_uart/software_uart.h"
+
+#else // TESTING
+#include "testing_helper.h"
+#include <avr-bootloader-common/crc32.h>
+#endif // TESTING
+
+#include <stdint.h>
+
+#ifdef __AVR__
+#include <avr/pgmspace.h>
+#else
+#define PROGMEM
+#endif //__AVR__
--- mcu/uartboot.h
+++ mcu/uartboot.h
@@ -2,26 +2,6 @@
 
 #include "config.h"
 
-#ifndef TESTING
-#include <avr-bootloader-common/all.h>
-#include <util/delay.h>
-
-#include "software_uart/software_uart.h"
-
-#else // TESTING
-#include "testing_helper.h"
-#endif // TESTING
-
-#include <avr-bootloader-common/crc32.h>
-
-#include <stdint.h>
-
-#ifdef __AVR__
-#include <avr/pgmspace.h>
-#else
-#define PROGMEM
-#endif //__AVR__
-
 constexpr uint8_t kStartOfHeading PROGMEM{0x01};
 constexpr uint8_t kWaitForDataStartBootloader PROGMEM{5};
 constexpr uint8_t kSizeOfFlashPage PROGMEM{128};
