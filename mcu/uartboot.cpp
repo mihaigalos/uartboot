90c74bad538d466bd5006d2124c0e5333e64fc38
Mihai Galos <mihaigalos@gmail.com>
2021-01-08 12:15:24 +0100

    Default testing implementations to separate file

--- mcu/uartboot.cpp
+++ mcu/uartboot.cpp
@@ -55,38 +55,3 @@ const Metadata UartBoot::decodeMetadata(const uint8_t (&in)[kMetadataSize]) cons
 {
     return *reinterpret_cast<const Metadata *>(in);
 }
-
-#ifdef TESTING
-
-void UartBoot::writePageBufferToFlash(const uint16_t address) const
-{
-    static_cast<void>(address);
-}
-void UartBoot::writeToPageBuffer(const uint16_t address, const uint8_t *data) const
-{
-    for (uint8_t i = 0; i < kPageSize; ++i)
-    {
-        emulated_flash_.data[address + i] = *data++;
-    }
-}
-
-void UartBoot::eraseApplication() const
-{
-    for (auto i = 0; i < kEmulatedFlashSize; ++i)
-    {
-        emulated_flash_.data[i] = kFlashUnprogrammedValue;
-    }
-}
-
-uint32_t UartBoot::readLatestApplicationTimestampFromInternalEeprom() const
-{
-    return 0xFFFFFFFF;
-}
-uint16_t UartBoot::readWordFromMetadata(uint16_t address) const
-{
-    return 0xFFFF;
-}
-
-uint8_t UartBoot::uart_read() { return 0xFF; }
-void UartBoot::_delay_ms(uint16_t) {}
-#endif
